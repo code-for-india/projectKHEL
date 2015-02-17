f26c9705da80d2344997a38c484f4d734fe72359
Raool <raool@rashi.inc>
2015-02-16 22:36:27 -0800

    Added DateDialog & RatingDialog

--- client/ProjectKhelApp/app/src/main/java/org/cfi/projectkhel/AttendanceActivity.java
+++ client/ProjectKhelApp/app/src/main/java/org/cfi/projectkhel/AttendanceActivity.java
@@ -1,5 +1,7 @@
 package org.cfi.projectkhel;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -11,18 +13,28 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class AttendanceActivity extends ActionBarActivity implements AdapterView.OnItemClickListener{
 
   private ListView mainList;
+  private Dialog ratingDialog;
+
   private MyCustomListAdapter listAdapter;
   private List<MyCustomData> mData = new ArrayList<>();
+  private int mYear;
+  private int mMonthOfYear;
+  private int mDayOfMonth;
+  private float userRankValue;
 
   private static int [] IMAGES = { R.drawable.android_calendar,
                             R.drawable.android_earth,
@@ -42,13 +54,24 @@ public class AttendanceActivity extends ActionBarActivity implements AdapterView
     mainList = (ListView) findViewById(R.id.listView1);
     mainList.setOnItemClickListener(this);
 
-    addInitialListContents();
+    initialize();
+
     // Set our custom adapter for the listView.
     listAdapter = new MyCustomListAdapter();
     mainList.setAdapter(listAdapter);
     listAdapter.refresh();
   }
 
+  private void initialize() {
+    populateListContents();
+
+    final Calendar c = Calendar.getInstance();
+    mYear = c.get(Calendar.YEAR);
+    mMonthOfYear = c.get(Calendar.MONTH);
+    mDayOfMonth = c.get(Calendar.DAY_OF_MONTH);
+
+    userRankValue = 3.0f;
+  }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
@@ -81,7 +104,20 @@ public class AttendanceActivity extends ActionBarActivity implements AdapterView
 
   @Override
   public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-
+    Log.d(MainActivity.TAG, "Clicked on position: " + position + " id = " + id);
+    switch(position) {
+      case 0:
+        handleDateDialog(mData.get(position));
+        break;
+      case 5:
+        handleRatingDialog(mData.get(position));
+        break;
+      case 2:
+        break;
+      default:
+        break;
+    }
+    listAdapter.refresh();
   }
 
   /**
@@ -157,9 +193,9 @@ public class AttendanceActivity extends ActionBarActivity implements AdapterView
     }
   }
 
-  private void addInitialListContents() {
+  private void populateListContents() {
     for (int i = 0; i < LABELS.length; i++) {
-      mData.add(new MyCustomData(IMAGES[i], LABELS[i], ""));
+      mData.add(new MyCustomData(IMAGES[i], LABELS[i], ""+i));
     }
   }
 
@@ -181,5 +217,43 @@ public class AttendanceActivity extends ActionBarActivity implements AdapterView
       content = myContent;
     }
 
+  }
+
+  private void handleDateDialog(final MyCustomData dataRow) {
+    final DatePickerDialog dpd = new DatePickerDialog(this,
+      new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+          dataRow.setContent(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+          mYear = year;
+          mMonthOfYear = monthOfYear;
+          mDayOfMonth = dayOfMonth;
+        }
+      }, mYear, mMonthOfYear, mDayOfMonth);
+    dpd.show();
+  }
+
+  private void handleRatingDialog(final MyCustomData dataRow) {
+    if (ratingDialog == null) {
+      ratingDialog = new Dialog(AttendanceActivity.this, R.style.NoTitleDialog);
+      ratingDialog.setContentView(R.layout.rating_dialog);
+      ratingDialog.setCancelable(true);
+    }
+
+    final RatingBar ratingBar = (RatingBar)ratingDialog.findViewById(R.id.dialog_ratingbar);
+    ratingBar.setRating(userRankValue);
+
+    final Button updateButton = (Button) ratingDialog.findViewById(R.id.rank_dialog_button);
+    updateButton.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        userRankValue = ratingBar.getRating();
+        dataRow.setContent(Float.toString(userRankValue));
+        ratingDialog.dismiss();
+      }
+    });
+    //now that the dialog is set up, it's time to show it
+    ratingDialog.show();
   }
 }
